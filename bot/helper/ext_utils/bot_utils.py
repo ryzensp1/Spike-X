23840fffca3a5317c5f68807cdc4040e4d84794a
ryzensp1 <116732740+ryzensp1@users.noreply.github.com>
2022-12-29 23:02:46 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -181,7 +181,8 @@ def get_readable_message():
             smsg += f"\n<b> <a>━━━━━━━━━━━━━━━━━━━</a> </b>"
             smsg += "\n\n"
         for index, download in enumerate(list(download_dict.values())[COUNT:], start=1):
-            msg += f"<code>{escape(str(download.name()))}</code>"
+            msg += f"<b>{escape(str(download.name()))}</b>"
+            smsg += "\n"
             msg += f"<b>┌ <a href='{download.message.link}'>{download.status()}</a>: </b>"
             if download.status() not in [MirrorStatus.STATUS_SEEDING, MirrorStatus.STATUS_SPLITTING]:
                 if EMOJI_THEME is True:
