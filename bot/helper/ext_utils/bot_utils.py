5a71fbe7b84fcba7a86b61fc1857e975e1fe6e40
ryzensp1 <116732740+ryzensp1@users.noreply.github.com>
2022-12-29 21:47:09 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -176,7 +176,7 @@ def get_readable_message():
             if PAGE_NO > PAGES and PAGES != 0:
                 globals()['COUNT'] -= STATUS_LIMIT
                 globals()['PAGE_NO'] -= 1
-        if EMOJI_THEME is True:
+        if STATUS_LIMIT is not None:
             smsg = f"\n<b> <a> ☲    CRIMZ X CLOUD ☁️     ☲ </a> </b>"
             smsg += f"\n<b> <a>━━━━━━━━━━━━━━━━━━━</a> </b>"
         for index, download in enumerate(list(download_dict.values())[COUNT:], start=1):
